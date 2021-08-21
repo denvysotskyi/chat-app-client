13393906505a6d20c39fa5e7a4c060841a552156
denvysotskyi <denvysotskyi@gmail.com>
2021-08-21 16:21:32 +0300

    Add AuthForm

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,34 +1,8 @@
-import { FC } from 'react'
-import styled from 'styled-components'
-import Header from '../header/Header'
 import Auth from '../auth/Auth'
-
-const AppWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-`
-const Container = styled.div`
-  width: 1440px;
-  display: grid;
-  grid-template-areas:
-  'header'
-  'content';
-`
-const Content = styled.div`
-  grid-area: content;
-  min-height: 700px;
-`
+import { FC } from 'react'
 
 const App: FC = () => (
-  <AppWrapper>
-    <Container>
-      <Header />
-      <Content>
-        <Auth />
-      </Content>
-    </Container>
-  </AppWrapper>
+      <Auth isAuth={true} />
 )
 
 export default App
--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -1,120 +1,44 @@
 import { FC } from 'react'
 import styled from 'styled-components'
-import { Formik, Form, Field } from 'formik'
-import * as Yup from 'yup'
+import Header from '../header/Header'
+import AuthForm from './AuthForm'
+import { IAuth } from '../../types/data'
 
-const Wrapper = styled.div`
-  margin-top: 10px;
+const AppWrapper = styled.div`
   display: flex;
   justify-content: center;
+  height: 100vh;
 `
-const AuthForm = styled(Form)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 290px;
-  min-height: 195px;
-  border: 1px solid black;
-  border-radius: 15px;
-  background: darkviolet;
+const Container = styled.div`
+  width: 1440px;
+  display: grid;
+  grid-template-areas:
+  'header'
+  'content';
 `
-const RoomField = styled(Field)`
-  margin: 8px 0 15px 0;
-  padding-left: 10px;
-  width: 200px;
-  height: 30px;
-  border-radius: 8px;
-  border: none;
-  outline: none;
+const Content = styled.div`
+  grid-area: content;
+  min-height: 700px;
 `
-const NameField = styled(Field)`
-  margin: 8px 0 15px 0;
-  padding-left: 10px;
-  width: 200px;
-  height: 30px;
-  border-radius: 8px;
-  border: none;
-  outline: none;
-`
-const Button = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 11px;
-  width: 80px;
-  height: 30px;
-  border-radius: 8px;
-  border: none;
-  outline: none;
-  text-transform: uppercase;
-  background: white;
-  &:hover {
-    cursor: pointer;
-    transform: translateY(3px);
-    transition: .3s all ease;
+
+const Auth: FC<IAuth> = ({ isAuth }) => {
+
+  if (isAuth) {
+    return <AppWrapper>
+             <Container>
+               <Header />
+               <Content></Content>
+             </Container>
+           </AppWrapper>
   }
-`
-const Error = styled.div`
-  font-size: 12px;
-  color: white;
-`
-
-const SignupSchema = Yup.object().shape({
-  room: Yup.string()
-    .min(2, 'Поле должно содержать минимум 2 символа!')
-    .required('Введите ваши данные'),
-  name: Yup.string()
-    .min(2, 'Поле должно содержать минимум 2 символа!')
-    .required('Введите ваши данные')
-})
-
-const Auth: FC = () => (
-
-  <Wrapper>
-    <Formik
-      initialValues={{
-        room: '',
-        name: '',
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={(values, {setSubmitting, resetForm}) => {
-        console.log(values.room, values.name)
-        setSubmitting(false)
-        resetForm()
-      }}
-    >
-      {({ errors, touched }) => (
-        <AuthForm>
-          {
-            errors.room && touched.room
-              ? (<Error>
-                {errors.room}
-              </Error>)
-              : null
-          }
-          <RoomField name={'room'}
-                 type={'text'}
-                 placeholder={'Введите Room ID'}
-          />
-          {
-            errors.name && touched.name
-              ? (<Error>
-                {errors.name}
-              </Error>)
-              : null
-          }
-          <NameField name={'name'}
-                 type={'text'}
-                 placeholder={'Введите ваше имя'}
-          />
-          <Button type={'submit'}>
-            Войти
-          </Button>
-        </AuthForm>
-      )}
-    </Formik>
-  </Wrapper>
-)
+  return <AppWrapper>
+           <Container>
+             <Header />
+             <Content>
+               <AuthForm />
+             </Content>
+           </Container>
+         </AppWrapper>
+}
 
 export default Auth
